4a806ba31419e3f558d171d10ce908af877736f9
victoria6991 <miss.victoria-liar@yandex.ru>
2022-05-11 13:13:20 +0400

    logic for date added

--- src/test/java/ru/netology/DeliveryTesting.java
+++ src/test/java/ru/netology/DeliveryTesting.java
@@ -2,18 +2,30 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTesting {
+
+    LocalDate date = LocalDate.now();
+
     @Test
     public void shouldSubmitForm(){
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
-        $("[placeholder=\"Дата встречи\"]").setValue("18.05.2022");
+        String month = "0";
+        if (date.getMonthValue() <= 9) {
+            month = "0" + String.valueOf(date.getMonthValue());
+        } else {
+            month = String.valueOf(date.getMonthValue());
+        }
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.CONTROL + "a" + Keys.DELETE);
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf((date.getDayOfMonth() + 5)) + month + (String.valueOf(date.getYear())));
         $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
         $("[name=phone]").setValue("+79157809999");
         $(".checkbox__box").click();
