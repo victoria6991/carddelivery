e61fb9adc11da34e00ffe693a9a220d5334f32f3
victoria6991 <miss.victoria-liar@yandex.ru>
2022-05-11 14:50:20 +0400

    2я задача

--- src/test/java/ru/netology/DeliveryTesting.java
+++ src/test/java/ru/netology/DeliveryTesting.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exactValue;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTesting {
 
@@ -26,6 +27,21 @@ public class DeliveryTesting {
         }
         $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.CONTROL + "a" + Keys.DELETE);
         $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf((date.getDayOfMonth() + 5)) + month + (String.valueOf(date.getYear())));
+        $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        $("[name=phone]").setValue("+79157809999");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=\"notification\"]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void shouldSubmitForm1(){
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Мо");
+        $x("//*[text()='Москва']").click();
+        $(".icon-button__text").click();
+        int newDate = date.getDayOfMonth() + 7;
+        $$(".calendar__day").find(exactText(String.valueOf(newDate))).click();
         $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
         $("[name=phone]").setValue("+79157809999");
         $(".checkbox__box").click();
